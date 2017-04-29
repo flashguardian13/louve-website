92c4be886e0c884d56278c17a8f4f17b15982f86
Ben "Flash" Amos <flashguardian13@gmail.com>
2017-04-29 09:59:48 -0700

    Fixed title on non-static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    parts = ['Rhiannon Louve']
+    parts.unshift(page_title) unless page_title.empty?
+    parts.join(' | ')
+  end
 end
