98bccfc1fe7f6b52da896b4af85d017fb389466e
Ben "Flash" Amos <flashguardian13@gmail.com>
2017-05-02 00:34:54 -0700

    Partial user lifecycle implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params.merge(is_admin: false))
+    if @user.save
+      flash[:success] = "User created successfully."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
