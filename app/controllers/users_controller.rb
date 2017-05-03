874f0c5342f04b410c0f46991992b2e992570c8b
Ben "Flash" Amos <flashguardian13@gmail.com>
2017-05-03 01:24:13 -0700

    Added user administration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,64 @@
 class UsersController < ApplicationController
+  before_action :redirect_unless_admin
+
   def index
-    redirect_to root_path unless logged_in? && current_user.is_admin
     @users = User.all
   end
 
   def show
-    redirect_to root_path unless logged_in? && current_user.is_admin
     @user = User.find(params[:id])
   end
 
   def new
-    redirect_to root_path unless logged_in? && current_user.is_admin
     @user = User.new
   end
 
   def create
-    redirect_to root_path unless logged_in? && current_user.is_admin
     @user = User.new(user_params.merge(is_admin: false))
     if @user.save
-      flash[:success] = "User created successfully."
+      flash[:success] = "User #{@user.name} created successfully."
       redirect_to @user
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.authenticate(params[:user][:password])
+      if @user.update_attributes(user_params)
+        flash[:success] = "User #{@user.name} updated successfully."
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    else
+      @user.errors.add(:password, "was incorrect.")
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    log_out if current_user == user
+    user.destroy
+    flash.now[:success] = "User #{user.name} deleted."
+
+    @users = User.all
+    render 'index'
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def redirect_unless_admin
+    redirect_to root_path unless logged_in? && current_user.is_admin
   end
 end
