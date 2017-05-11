f5bb030bff8dbd40d2d38a9e23bb5acd437779fc
Ben "Flash" Amos <flashguardian13@gmail.com>
2017-05-11 00:58:23 -0700

    Consolidated common controller functionality to SuperController

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,54 +1,7 @@
-class BlogPostsController < ApplicationController
-  before_action :redirect_unless_admin, except: [:index]
-
-  def index
-    @contents = model_class.all
-  end
-
-  def new
-    @content = model_class.new
-  end
-
-  def create
-    @content = model_class.new(content_params)
-    if params[:commit] != 'Preview' && @content.save
-      redirect_to action: :index, refresh: true
-    else
-      render 'new'
-    end
-  end
-
-  def edit
-    @content = model_class.find(params[:id])
-  end
-
-  def update
-    @content = model_class.find(params[:id])
-    if params[:commit] == 'Preview'
-      @content.assign_attributes(content_params)
-      render 'edit'
-    elsif @content.update_attributes(content_params)
-      redirect_to action: :index, refresh: true
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    content = model_class.find(params[:id])
-    content.destroy
-
-    @contents = model_class.all
-    render 'index'
-  end
-
+class BlogPostsController < ContentsController
   private
 
   def content_params
     params.require(:blog_post).permit(:title, :content, :is_visible)
-  end
-
-  def model_class
-    controller_name.classify.constantize
   end
 end
---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,50 @@
+class ContentsController < ApplicationController
+  before_action :redirect_unless_admin, except: [:index]
+
+  def index
+    @contents = model_class.all
+  end
+
+  def new
+    @content = model_class.new
+  end
+
+  def create
+    @content = model_class.new(content_params)
+    if params[:commit] != 'Preview' && @content.save
+      redirect_to action: :index, refresh: true
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @content = model_class.find(params[:id])
+  end
+
+  def update
+    @content = model_class.find(params[:id])
+    if params[:commit] == 'Preview'
+      @content.assign_attributes(content_params)
+      render 'edit'
+    elsif @content.update_attributes(content_params)
+      redirect_to action: :index, refresh: true
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    content = model_class.find(params[:id])
+    content.destroy
+
+    @contents = model_class.all
+    render 'index'
+  end
+
+  private
+
+  def model_class
+    controller_name.classify.constantize
+  end
+end
