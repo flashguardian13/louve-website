7c1b50833d8ec80eff72705659e82be6af2991c9
Ben "Flash" Amos <flashguardian13@gmail.com>
2017-07-04 21:54:21 -0700

    First visible blog and publication now show on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    @publication = Publication.all.select { |p| p.is_visible }.first
+    @blog_post = BlogPost.all.select { |b| b.is_visible }.first
   end
 
   def about
