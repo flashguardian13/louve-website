6d8766007e31e94ed96852f8798d94d2e6c672c3
Ben "Flash" Amos <flashguardian13@gmail.com>
2019-04-12 18:13:03 -0700

    Common logged-in usability tests

--- spec/selenium/common_spec.rb
+++ spec/selenium/common_spec.rb
@@ -16,14 +16,14 @@ describe 'Common Elements' do
 
   it 'has six navigation buttons at the top and bottom' do
     ['header > div.navbar > nav > ul a', 'footer > div.navbar > nav > ul a'].each do |selector|
-      header_nav_links = @driver.find_elements(css: selector)
-      expect(header_nav_links.length).to eq(6)
+      nav_links = @driver.find_elements(css: selector)
+      expect(nav_links.length).to eq(6)
 
-      link_texts = header_nav_links.map { |b| b.text }
+      link_texts = nav_links.map { |b| b.text }
       expected = %w(Home About Publications Blogs Links Contact)
       expect(link_texts).to match_array(expected)
 
-      link_paths = header_nav_links.map { |b| b['href'] }
+      link_paths = nav_links.map { |b| b['href'] }
       expected = [
         'http://localhost:3000/',
         'http://localhost:3000/about',
@@ -61,6 +61,45 @@ describe 'Common Elements' do
       wait_for_page_load
       expect(@driver.title).to eq(title)
       expect(@driver.current_url).to eq(url)
+    end
+  end
+
+  context 'when signed in' do
+    before(:all) do
+      login_as_admin(@driver)
+      @driver.navigate.to "http://localhost:3000/"
+    end
+
+    after(:all) do
+      logout(@driver)
+      @driver.navigate.to "http://localhost:3000/"
+    end
+
+    it 'displays the username' do
+      element = @driver.find_element(css: 'header > div#login-info')
+      expect(element.text).to include('signed in')
+      expect(element.text).to include('admin')
+    end
+
+    it 'displays a users button' do
+      nav_links = @driver.find_elements(css: 'header > div.navbar > nav > ul a')
+      expect(nav_links.length).to eq(7)
+
+      link_texts = nav_links.map { |b| b.text }
+      expected = %w(Home About Publications Blogs Links Contact Users)
+      expect(link_texts).to match_array(expected)
+
+      link_paths = nav_links.map { |b| b['href'] }
+      expected = [
+        'http://localhost:3000/',
+        'http://localhost:3000/about',
+        'http://localhost:3000/publications',
+        'http://localhost:3000/blogs',
+        'http://localhost:3000/links',
+        'http://localhost:3000/contact',
+        'http://localhost:3000/users'
+      ]
+      expect(link_paths).to match_array(expected)
     end
   end
 end
